32b18224d16b5ba7db083c61d44db074cc67a03b
elias4ty <alytray718@sina.com>
2017-11-13 23:39:07 +0800

    path

--- face/config.py
+++ face/config.py
@@ -1,5 +1,5 @@
 config = {
-    'mac' : '/Users/elias/Documents/python_exercise/flask_01',
+    'mac' : '/Users/elias/Documents/python_exercise/flask_01/face/input',
     'production' : '/home/ai_demo/src/face/input/'
 }
 
