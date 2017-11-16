3c0e40063377dd4f72a6e8755fe2374f66cdc06e
tangyang <tangyang@sogou-inc.com>
2017-11-16 10:19:59 +0800

    win

--- face/config.py
+++ face/config.py
@@ -1,6 +1,7 @@
 config = {
     'mac' : '/Users/elias/Documents/python_exercise/flask_01/face/input',
-    'production' : '/home/ai_demo/src/face/input/'
+    'production' : '/home/ai_demo/src/face/input/',
+    'win' : 'E:/exercise/python_exercise/input'
 }
 
 def dic(arg):
